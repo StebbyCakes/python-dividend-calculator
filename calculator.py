acd89c6cadc06410ebda8142bf994d672a8efef8
Esteban Carter <carterea1999@gmail.com>
2021-08-23 19:43:03 -0400

    Working on creating variables

---
+++ calculator.py
@@ -0,0 +1,6 @@
+initial_investment = int(input('Initial investment: '))
+yearly_investment = int(input('Yearly investment: '))
+yield_investment = int(input('Average portfolio yield: '))
+years = int(input('How many years will you invest: '))
+
+capitol = initial_investment + yearly_investment
