598107e9ec7305e7395ab3fe8c52b7c074daf696
Esteban Carter <carterea1999@gmail.com>
2021-08-24 15:42:25 -0400

    working dividend calculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 initial_investment = int(input('Initial investment: '))
 yearly_investment = int(input('Yearly investment: '))
 yield_investment = int(input('Average portfolio yield: '))
+yield_increment = int(input('By what percent will the yield increase per year: '))
 years = int(input('How many years will you invest: '))
 
-capitol = initial_investment + yearly_investment
+
+
+for i in range(0, years):
+    if ( i < 1):
+        capitol = initial_investment + yearly_investment
+        dividend = (capitol * (yield_investment / 100))
+        yearly_capitol = capitol + dividend
+        print(f"Capitol: ${yearly_capitol} Dividend: ${dividend} yield: {yield_investment}%")
+        yearly_yield = yield_investment * (yield_increment / 100) + yield_investment
+    else:
+        dividend = (yearly_capitol * (yearly_yield / 100))
+        yearly_capitol = yearly_capitol + dividend + yearly_investment
+        print(f"Capitol: ${yearly_capitol} Dividend: ${dividend} yield: {yearly_yield}%")
+        yearly_yield = yearly_yield * (yield_increment / 100) + yearly_yield
