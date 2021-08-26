27c9289fe1e27ef591de25e411df04356665d156
Esteban Carter <carterea1999@gmail.com>
2021-08-26 17:47:07 -0400

    Visual for how much has been paid out in dividends

--- calculator.py
+++ calculator.py
@@ -5,25 +5,30 @@ yield_investment = int(input('Average portfolio yield: '))
 yield_increment = int(input('Percent the yield increases per year: '))
 years = int(input('How many years will you invest: '))
 
-current_year = 0
+current_year = 1
+dividend_total = 0
 
 
 
 for i in range(0, years):
     if ( i < 1):
-        current_year += 1
         capitol = initial_investment + yearly_investment
         dividend = (capitol * (yield_investment / 100))
+        dividend_total += dividend
         yearly_capitol = (((capitol + dividend) * (yearly_increase / 100)) + (capitol + dividend))
         formatted_yearly_capitol = "{:.2f}".format(yearly_capitol)
         print(f" Year: {current_year} Capitol: ${formatted_yearly_capitol} Dividend: ${dividend} yield: {yield_investment}%")
         yearly_yield = yield_investment * (yield_increment / 100) + yield_investment
+
+
     else:
         current_year += 1
         dividend = (yearly_capitol * (yearly_yield / 100))
+        dividend_total += dividend
         yearly_capitol = (((yearly_capitol + dividend + yearly_investment) * (yearly_increase / 100)) + (yearly_capitol + dividend + yearly_investment))
         formatted_yearly_capitol = "{:.2f}".format(yearly_capitol)
         formatted_dividend = "{:.2f}".format(dividend)
         formatted_yearly_yield = "{:.2f}".format(yearly_yield)
         print(f"Year: {current_year} Capitol: ${formatted_yearly_capitol} Dividend: ${formatted_dividend} yield: {formatted_yearly_yield}%")
+        print(dividend_total)
         yearly_yield = yearly_yield * (yield_increment / 100) + yearly_yield
